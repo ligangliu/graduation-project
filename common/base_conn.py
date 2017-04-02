b94f7201381d67d4f9f4e116ee2fd30137eaad8d
liu <528762404@qq.com>
2017-04-02 18:05:43 +0800

    add common main.py venv

---
+++ common/base_conn.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-04-02 15:46:02
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
+from pymongo import MongoClient
+
+mongo_uri_auth = "mongodb://localhost:27017/"
+database_name = "mydb"
+collection_name = "person"
+
+def get_db(mongo_uri_auth=mongo_uri_auth,database_name=database_name):
+	client = MongoClient(mongo_uri_auth)
+	db = client[database_name]
+	return db
+def get_collection_names(mongo_uri_auth=mongo_uri_auth,database_name=database_name):
+	db = get_db()
+	return db.collection_names(include_system_collections=False)
+
+#print get_conn()
+#print get_collection_names()
---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-04-02 15:49:52
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
