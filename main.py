5fbf5dbb3dd65f09f9585c62f91c3af44616d3ed
liu <528762404@qq.com>
2017-04-04 12:34:57 +0800

    modfily main.py

--- main.py
+++ main.py
@@ -6,3 +6,12 @@
 # @Version : $Id$
 
 import os
+from flask import Flask,jsonify
+import datetime
+from common import base_conn
+
+app = Flask(__name__)
+print base_conn.get_collection_names()
+
+if __name__ == '__main__':
+	app.run()
